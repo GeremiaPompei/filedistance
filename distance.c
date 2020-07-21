72e3605ad707bdd58dd7bf568106ec1dcf47bcfd
root <geremia.pompei@studenti.unicam.it>
2020-07-21 14:18:46 -0400

    Sistemato quasi tutto

--- distance.c
+++ distance.c
@@ -63,7 +63,7 @@ void calculate_distance(char *contentf1, char *contentf2, int **matrix){
 void file_m_build(int **matrix, int sizef1, int sizef2, char *file1, char *file2, char *output){
     int i;
     int size = matrix[sizef1][sizef2] + 1;
-    char **instructions = malloc_matrix(size+1,sizeof(char *) * size,sizeof(char ) * 50);
+    char **instructions = malloc_matrix(size + 1,sizeof(char *) * (size + 1),sizeof(char ) * 9);
     FILE *file = fopen(output,"w");
     if(file==NULL)
         exit(1);
@@ -96,6 +96,6 @@ void file_m_build(int **matrix, int sizef1, int sizef2, char *file1, char *file2
     for (i = 1;i<size;i++) {
         fprintf(file, "%s", instructions[i]);
     }
-    free_matrix(instructions,size+1);
+    free_matrix(instructions,size + 1);
     fclose(file);
 }
